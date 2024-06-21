7b4ad90b5d9d1c3137877a93280db3e4844bb00b
Veisha <118648494+veisha@users.noreply.github.com>
2024-06-22 00:56:25 +0800

    Database added
    -DONE-
    Task load
    Task insert save
    
    -TO DO-
    A LOT!!!
    Fix all of the CRUD
    for all the tables
    Add update and delete for Tasks Table
    For the rest it needs to update all of their CRUD

---
+++ public/server/db/connection.js
@@ -0,0 +1,19 @@
+// db/connection.js
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'evanpogi13',
+    database: 'CompanyDB'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
+module.exports = connection;
